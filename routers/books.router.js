23f3184b8b85c130e65bc02b3e3995096246a9d7
camilomedisar <79817981+camilomedisar@users.noreply.github.com>
2023-06-20 17:17:33 -0500

    point d,h,j

--- routers/books.router.js
+++ routers/books.router.js
@@ -6,12 +6,23 @@ const {
     deleteBook,
     updateBook,
     getBookById,
+    SortByBookName,
+    SortByCategory,
+    SortByAuthor,
+    getBorrowedBooksByUserId,
+    calculateOverdueFees,
+    getBookByName
 } = require("../controllers/books.controller");
 
 router.get('/', getAllBooks);
 router.post('/', createBook);
 router.delete('/:book_id', deleteBook);
 router.put('/:book_id', updateBook);
+router.get('/BookName', SortByBookName);
+router.get('/Category', SortByCategory);
+router.get('/Author', SortByAuthor);
 router.get('/:book_id', getBookById);
-
+router.get('/borrow/:filterParam', getBorrowedBooksByUserId);
+router.get('/fee/:filterParam', calculateOverdueFees);
+router.get('/filter/:filterParam', getBookByName);
 module.exports = router;
